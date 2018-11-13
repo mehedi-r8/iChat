c72e1cfa60522d31095d61e5b847e695a9608a14
Mehedi Right <mehedi.r8@mehedir8s-macbook-air.local>
2018-11-13 22:45:55 +0600

    Initial Commit

---
+++ iChat/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  iChat
+//
+//  Created by MEHEDI.R8 on 11/13/18.
+//  Copyright © 2018 R8soft. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ iChat/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  iChat
+//
+//  Created by MEHEDI.R8 on 11/13/18.
+//  Copyright © 2018 R8soft. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
